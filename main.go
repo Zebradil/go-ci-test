f30cf1ef85cd7c9de41646c0c1a1f8860caae13a
German Lashevich <german.lashevich@gmail.com>
2023-06-02 18:45:21 +0200

    feat: Print version information

--- main.go
+++ main.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+)
+
 func main() {
 	fmt.Println("🫂 Hello, World!")
+	fmt.Println("----------------")
+	fmt.Printf("version: %s\n", version)
+	fmt.Printf("commit: %s\n", commit)
+	fmt.Printf("date: %s\n", date)
 }
